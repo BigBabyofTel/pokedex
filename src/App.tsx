df326798cc153b68dbb963da7dd30cc6339cf470
bigbabyoftel <augustus.tb@outlook.com>
2024-01-28 18:33:09 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+    <div className="border-4 mt-5 flex justify-center">
+      <h1 className="text-5xl">PKdex</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/MainDisplay.tsx
@@ -0,0 +1,12 @@
+export default function MainDisplay() {
+
+    
+
+    return (
+        <>
+        <div>
+            <h1>Test 2</h1>
+        </div>
+        </>
+    )
+}
