e06640a77f5ad566fafac5daba456c3186a1c948
bigbabyoftel <augustus.tb@outlook.com>
2024-01-28 18:54:38 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <div className="border-4 mt-5 flex justify-center">
-      <h1 className="text-5xl">PKdex</h1>
+      <h1 className="text-5xl">PKdex app</h1>
       </div>
     </>
   )
