a9c99066af8da9d49fe5ba5779b608972f8d0196
bigbabyoftel <augustus.tb@outlook.com>
2024-01-29 05:45:54 +0200

    pokemon api called

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import MainDisplay from "./assets/MainDisplay"
 
 function App() {
 
 
   return (
     <>
-    <div className="border-4 mt-5 flex justify-center">
-      <h1 className="text-5xl">PKdex app</h1>
+    <div className="border-2 m-5 p-3 flex justify-between">
+      <h1 className="text-xl">Pokedex App</h1>
+      <h2>Region:</h2>
+      <h3>time</h3>
       </div>
+      <MainDisplay />
     </>
   )
 }
--- src/assets/MainDisplay.tsx
+++ src/assets/MainDisplay.tsx
@@ -1,12 +1,44 @@
-export default function MainDisplay() {
+import { useState } from "react";
 
+export default function MainDisplay() {
+const [pokemon, setPokemon] = useState([]);
+
+function getRandomNumber():number {
+    return Math.floor(Math.random() * 151);
+}
+const url = `https://pokeapi.co/api/v2/pokemon/${getRandomNumber()}/`;
+const getPokemon = async(url:string) => {
+    try {
+        const request = new Request(url);
+        const response = await fetch(request);
+        const data = await response.json();
+        setPokemon(data);
+    } catch (error) {
+        console.error();
+    }
+};
+
+console.log(pokemon);
+
+;  
+/*
+const renderPokemon = () => {
+  try {
     
+    return pokemon.map(
+
+    )
+  } catch (error) {
+    console.error();
+  }  
+*/
 
     return (
         <>
-        <div>
-            <h1>Test 2</h1>
+        <div className="border-2">
+            <h1 className="p-5 underline underline-offset-8">Pokedex No. : </h1>
+
         </div>
         </>
     )
-}
+};
