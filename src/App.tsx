dc2736bf9e11eafb8757fcaa07f33bab8267104c
BigBabyofTel <augustus.tb@outlook.com>
2024-04-11 23:25:30 +0200

    FEAT: semantic structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import MainDisplay from "./assets/MainDisplay"
+import { Fragment } from "react";
 
-function App() {
-
-
+export default function App() {
   return (
-    <>
-    <div className="border-2 m-5 p-3 flex justify-between">
-      <h1 className="text-xl">Pokedex App</h1>
-      <h2>Region:</h2>
-      <h3>time</h3>
-      </div>
-      <MainDisplay />
-    </>
-  )
+    <Fragment>
+      <header className="text-6xl border-4">
+        <h1>PokeDex</h1>
+      </header>
+      <nav>
+        <ul className="border-4">
+          <li>Search</li>
+          <li>My Pokemon</li>
+          <li>Trading cards</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
+      <main className="border-4">
+        <h2>Pokemon</h2>
+        <article className="border-4">
+          <h2>picture of pokemon</h2>
+          <aside className="border-4">pokemon stats</aside>
+          <figure className="border-4">evolutions and details</figure>
+          <figure className="border-4">moves</figure>
+        </article>
+      </main>
+      <footer className="border-4"><h2>Created by T. Augustus Baker</h2></footer>
+    </Fragment>
+  );
 }
-
-export default App
